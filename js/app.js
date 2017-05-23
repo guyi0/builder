a49b7563d39db7470d9c47821f7f853f4bf3d4f2
guy iluz <guyi@herep.co.uk>
2017-05-23 13:23:18 +0300

    Fix grid

--- js/app.js
+++ js/app.js
@@ -4,29 +4,24 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import 'waypoints';
 import 'scrollTo';
 
+
+
+
+$('.dim').click(function(event){
+    var isDim = $(event.target).is('.dim');
+    if(isDim){ //make sure I am a dim element
+        $('.checkbox', this).toggle(); // p00f
+    }
+
+});
+
+
+
+
 /**
  * EXAMPLE JS STARTS
  */
-$(function() {
-    $('[id^=scrollTo]').click(function() {
-        var id = $(this).attr('id').slice(9);
-        $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
-    });
 
-    $('#marketing').waypoint(function() {
-        $('.img-circle').addClass('animated zoomIn');
-    }, {
-        offset: '50%',
-        triggerOnce: true
-    });
-
-    $('.featurette').waypoint(function() {
-        $('#' + this.element.id + ' .featurette-image').addClass('animated pulse');
-    }, {
-        offset: '50%',
-        triggerOnce: true
-    });
-});
 /**
  * EXAMPLE JS ENDS
  */
