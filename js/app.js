61bdf9570434d123113aac35532e288df80514f7
guy iluz <guyi@herep.co.uk>
2017-05-29 09:41:06 +0300

    add nabe

--- js/app.js
+++ js/app.js
@@ -4,8 +4,55 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import 'waypoints';
 import 'scrollTo';
 
+$( document ).ready(function() {
+  history.pushState(null, null, 'home')
+  var fetchAndInsert,body
+var loc =[""]
+
+var fetchAndInsert = function (href) {
+  $.ajax({
+  url:'http://localhost:8080/view/'+href.split('/').pop(),
+  cache:false,
+  success:function(data) {
+    $("#main").html(data)
+    history.pushState(null, null, href)
+  }
+  })
+}
+$(window).on('popstate', function () {
+var lasturl =loc[loc.length-2]
+if (lasturl==undefined) {
+  fetchAndInsert("/")
+}
+console.log("lasturl",lasturl);
+fetchAndInsert(lasturl)
+
+})
+
+var nav =$('.nav')
+
+nav.find('a').on('click',function (event) {
+var href= $(this).attr('href')
+
+// mainpulte history
+history.pushState(null,null, href)
+
+loc.push(location.pathname)
+// fetch and chages
+
+fetchAndInsert(href)
+event.preventDefault()
+
+})
 
 
+// user goes back
+
+
+
+
+
+});
 
 $('.dim').click(function(event){
     var isDim = $(event.target).is('.dim');
@@ -15,9 +62,9 @@ $('.dim').click(function(event){
 
 });
 
-$( "#profieP" ).click(function() {
-  $( "#main" ).load( "profilePages.html" );
-});
+
+// hisotry
+
 
 
 /**
